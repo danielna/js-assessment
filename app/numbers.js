72ceedb90446117b7c81fc0b321e7a77cbf12757
Daniel Na <daniel.na@gmail.com>
2013-07-09 18:29:35 -0400

    Add logicalOperators, flowControl, numbers. Bitwise operators are a pain in the ass.

--- app/numbers.js
+++ app/numbers.js
@@ -3,19 +3,32 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-
+        var binary = num.toString(2);
+        return parseInt(binary.charAt(binary.length - bit), 10);
     },
 
     base10: function(str) {
-
+        return parseInt(str, 2);
     },
 
     convertToBinary: function(num) {
-
+        var output = num.toString(2);
+        while (output.length < 8) {
+            output = "0" + output;
+        }
+        return output;
     },
 
     multiply: function(a, b) {
+        function decLength(num) {
+            num = num.toString();
+            return num.length - 1 - num.indexOf('.');
+        }
+        var aL = decLength(a),
+            bL = decLength(b);
 
+        var length = (aL > bL) ? aL : bL;
+        return parseFloat((a * b).toFixed(length));
     }
   };
 });
