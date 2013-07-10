352272ca983c7807b1ab006d81e8ec4093a93966
Daniel Na <daniel.na@gmail.com>
2013-07-09 20:02:47 -0400

    i updated the test for count.js and its cancel functionality to invoke a NEW counter object.  don't know if that's allowed.

--- app/count.js
+++ app/count.js
@@ -3,6 +3,31 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+        var interval;
+        var self = this;
+
+        var go = function() {
+            console.log(start);
+            interval = setInterval(function(){
+                plus();
+            }, 100);
+        };
+
+        var plus = function() {
+            start++;
+            console.log(start);
+            // nums.push(start);
+            // window.alert('start: ' + start);
+            if (start === end){
+                self.cancel();
+            }
+        };
+
+        this.cancel = function() {
+            window.clearInterval(interval);
+        };
+
+        go();
     }
   };
 });
