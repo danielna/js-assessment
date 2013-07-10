36c08b55e4d9154d3f40810a6ec034226cec5396
Daniel Na <daniel.na@gmail.com>
2013-07-10 13:28:46 -0400

    get most of modules done, except for currying

--- app/modules.js
+++ app/modules.js
@@ -3,7 +3,14 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     createModule : function(str1, str2) {
+        // this.name = str1;
+        // this.greeting = str2;
 
+        // this.sayIt = function() {
+        //     return this.greeting + ", " + this.name;
+        // };
+
+        // return this;
     }
   };
 });
