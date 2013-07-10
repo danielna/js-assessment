d0a10be3b3b056f328f589d86df4a8470fcc7787
Daniel Na <daniel.na@gmail.com>
2013-07-10 13:29:21 -0400

    most of regex, except monetary

--- app/regex.js
+++ app/regex.js
@@ -3,26 +3,33 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+        var re = /\d/;
+        return re.test(str);
     },
 
     containsRepeatingLetter : function(str) {
-
+        // got this off the web; still don't understand the () or \1
+        var re = /([A-Za-z])\1/;
+        return re.test(str);
     },
 
     endsWithVowel : function(str) {
-
+        var re = /([aeiou])$/i;
+        return re.test(str);
     },
 
     captureThreeNumbers : function(str) {
-
+        var re = /\d{3}/;
+        var val = re.exec(str);
+        return (val) ? val[0] : false;
     },
 
     matchesPattern : function(str) {
-
+        var re = /\b\d\d\d-\d\d\d-\d\d\d\d$/;
+        return re.test(str);
     },
     isUSD : function(str) {
-
+        
     }
   };
 });
